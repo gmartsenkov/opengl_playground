a30077122d4dd622a489fd0464403ad0c34a12b2
Gogo <g.martsenkov@gmail.com>
2020-07-11 17:17:22 +0100

    create window and opengl context

---
+++ cmake-build-debug/CMakeFiles/OpenGLCourse.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gogo/CLionProjects/OpenGLCourse/main.cpp" "C:/Users/Gogo/CLionProjects/OpenGLCourse/cmake-build-debug/CMakeFiles/OpenGLCourse.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../dependencies/glew-2.1.0/include"
+  "../dependencies/glfw-3.3.2.bin.WIN32/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGLCourse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLCourse.dir/main.cpp.obj"
+  "OpenGLCourse.exe"
+  "OpenGLCourse.exe.manifest"
+  "OpenGLCourse.lib"
+  "OpenGLCourse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLCourse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
